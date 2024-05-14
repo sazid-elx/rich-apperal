11017fd67a9cd8483221d3483c6ba9ff0e2f4b15
sazid-elx <sazid@echologyx.com>
2024-05-14 17:11:10 +0600

    added blogs, dynamic header

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -1,5 +1,3 @@
-
-
 $('span#close-drawer').on('click', function () {
     $('.cart-drawer').removeClass('opened');
 });
@@ -14,44 +12,44 @@ $('#open-cart').on('click', function () {
                 detail: cart
             }));
 
-            console.log(cart);
+        console.log(cart);
+        $('.cart-count').text(cart.item_count);
 
-            const cartItemsHTML = cart.items.map(item => {
-                return `
-                    <div class="item flex gap-8" data-item-id="${item.id}">
-                        <img src="${item.image}" height="100px" width="100px" />
-                        <div>
-                            <h3>${item.title}</h3>
-                            <p>${item.variant_title}</p>
-                            <p>${item.quantity} x ${item.price} </p>
-                            <button class="remove-item-btn w-b-btn short" data-item-id="${item.id}">Remove</button>
-                        </div>
+        const cartItemsHTML = cart.items.map(item => {
+            return `
+                <div class="item flex gap-8" data-item-id="${item.id}">
+                    <img src="${item.image}" height="100px" width="100px" />
+                    <div>
+                        <h3>${item.title}</h3>
+                        <p>${item.variant_title}</p>
+                        <p>${item.quantity} x ${item.price} </p>
+                        <button class="remove-item-btn w-b-btn short" data-item-id="${item.id}">Remove</button>
                     </div>
-                `;
-            }).join("");
+                </div>
+            `;
+        }).join("");
 
-            const cartDrawerForm = document.querySelector('.cart-drawer-form .cart-items');
-            cartDrawerForm.innerHTML = cartItemsHTML;
+        const cartDrawerForm = document.querySelector('.cart-drawer-form .cart-items');
+        cartDrawerForm.innerHTML = cartItemsHTML;
 
-            $('.remove-item-btn').on('click', function(event) {
-                event.preventDefault();
-                const itemId = $(this).data('item-id');
-                console.log(itemId);
-                const removeUrl = `/cart/change?id=${itemId}&quantity=0`;
+        $('.remove-item-btn').on('click', function(event) {
+            event.preventDefault();
+            const itemId = $(this).data('item-id');
+            const removeUrl = `/cart/change?id=${itemId}&quantity=0`;
 
-                fetch(removeUrl, { method: 'POST' })
-    
-                    .then(() => {
-                        // $('#open-cart').click();
-                        let delItem = document.querySelector(`.cart-items .item[data-item-id="${itemId}"]`);
-                        console.log(delItem);
-                        delItem.remove();
-                    })
-                    .catch(error => console.error(error));
-            });
-
+            fetch(removeUrl, { method: 'POST' })
+                .then(() => fetch('/cart.js'))
+                .then(response => response.json())
+                .then(updatedCart => {
+                    let delItem = document.querySelector(`.cart-items .item[data-item-id="${itemId}"]`);
+                    delItem.remove();
+                    
+                    $('.cart-count').text(updatedCart.item_count);
+                })
+                .catch(error => console.error(error));
         });
+
+    });
 
     $('.cart-drawer').addClass('opened');
 });
-
